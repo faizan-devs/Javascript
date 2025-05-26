d54c1d0b8779ba013d5e9ff6785359e1dde59721
Faizan Ali <alammdfaizan50@gmail.com>
2025-05-26 12:47:27 +0530

    understanding empty string behaviour with different scenario

--- Lecture1/01_variables.js
+++ Lecture1/01_variables.js
@@ -12,3 +12,31 @@ state = "Maharashtra";
 city = "Khopoli";
 
 console.table([password, adminName, isLoggedInTime, adminLoggedIn, city, state, user]);
+
+//* Unnecessary outer loop plus return inside outer loop
+// function writeTable(num){
+//     let result = '';
+//     for(let i = num; i === num; i++){
+//         for(let j = 1; j <= 10; j++){
+//             // console.log(`${i} * ${j} = ${i*j}`);
+//             result += `${i} * ${j} = ${i*j}\n`;
+//         }
+//     return result;
+//     }
+// }
+
+// let number = writeTable(4);
+// console.log(number);
+
+
+//* Correct version more redable and not unecessary outer loop
+function writeTable(num){
+    let result = '';
+    for(let j = 1; j <= 10; j++){
+        result += `${num} * ${j} = ${num*j}\n`
+    }
+    return result;
+}
+
+const tableNumber = writeTable(6);
+console.log(tableNumber);
